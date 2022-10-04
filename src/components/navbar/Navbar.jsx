5a87a5b1b6132f50dceb04c6da49ce7300ee17cb
sandeepshrivastava13 <shrivastavasandeep13@gmail.com>
2022-10-05 01:34:51 +0530

    navbar work done

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,51 @@
 import React from 'react';
+import SearchIcon from '@mui/icons-material/Search';
 import './navbar.scss';
+import LanguageOutlinedIcon from '@mui/icons-material/LanguageOutlined';
+import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined';
+import FullscreenExitOutlinedIcon from '@mui/icons-material/FullscreenExitOutlined';
+import NotificationsNoneOutlinedIcon from '@mui/icons-material/NotificationsNoneOutlined';
+import ChatBubbleOutlineOutlinedIcon from '@mui/icons-material/ChatBubbleOutlineOutlined';
+import ListOutlinedIcon from '@mui/icons-material/ListOutlined';
+
 
 const Navbar = ()=>{
     return(
         <div className='navbar'>
-            navbar
+            <div className="wrapper">
+                <div className="search">
+                    <input type='text' placeholder='Search....'/>
+                    <SearchIcon />
+                </div>
+                <div className="items">
+                    <div className="item">
+                        <LanguageOutlinedIcon className='icon' />
+                        English
+                    </div>
+                    <div className="item">
+                        <DarkModeOutlinedIcon className='icon'/>
+                    </div>
+                    <div className="item">
+                       <FullscreenExitOutlinedIcon className='icon'/>
+                    </div>
+                    <div className="item">
+                        <NotificationsNoneOutlinedIcon className='icon'/>
+                        <div className="counter">1</div>               
+                     </div>
+                    <div className="item">
+                        <ChatBubbleOutlineOutlinedIcon className='icon' />
+                        <div className="counter">2</div>        
+                    </div>
+                    <div className="item">
+                        <ListOutlinedIcon className='icon'/>
+                    </div>
+
+                    <div className="item">
+                        <img src='https://akm-img-a-in.tosshub.com/indiatoday/images/story/202209/shah-rukh-khan-1-2_1200x768.jpeg?b8MYSWfocPIPjDvbI5Wq9R0xh5KT3S3t&size=770:433'alt='hello' className="avatar"/>
+                    </div>
+                </div>
+
+            </div>
         </div>
     )
 }
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
+import Navbar from '../../components/navbar/Navbar';
 import Sidebar from '../../components/sidebar/Sidebar';
+import Widget from '../../components/widget/Widget';
 import "./home.scss"
 
 const Home = ()=>{
     return(
         <div className='home'>
            <Sidebar />
-           <div className="homeContainer">Container</div>
+           <div className="homeContainer">
+           <Navbar />
+           <div className="widgets">
+            <Widget />
+           </div>
+           </div>
+          
+
         </div>
     )
 }
